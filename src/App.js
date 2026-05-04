526130dbbee2990149514bc736699a8d35e457a0
graciElla001 <onwuasoanyaijeoma1@gmail.com>
2026-05-04 09:51:20 +0100

    frontend initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
 
 function App() {
+  const [tasks, setTasks] = useState([]);
+
+  useEffect(() => {
+    fetch("http://localhost:5000/api/tasks")
+      .then(res => res.json())
+      .then(data => setTasks(data));
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Task App</h1>
+      <ul>
+        {tasks.map(t => (
+          <li key={t.id}>{t.task}</li>
+        ))}
+      </ul>
     </div>
   );
 }
